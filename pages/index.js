f94d944c5306109902877709dfb0e32802a24d77
EdwardGoomba <ask@edwardvdesign.com>
2019-03-03 14:21:34 -0500

    Setup page, basic info added

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+const Home = () => (
+  <div>
+    <h1>Welcome to dock90.</h1>
+    <h2>Strategy, design, development for SaaS teams.</h2>
+    <h3>hello@dock90.io</h3>
+  </div>
+);
+
+export default Home;
